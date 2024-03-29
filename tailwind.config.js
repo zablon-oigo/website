2e62a3669726bc84a7b94eca1dbf60c8e61ad1a4
N3v1 <n3v1010@gmail.com>
2024-03-29 13:15:46 +0100

    [Chore]: configure react

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: ["./src/**/*.{html,js}", "./scripts/**/*.js"],
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}",],
   theme: {
     extend: {},
   },
