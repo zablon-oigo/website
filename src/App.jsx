a17e9cae9c41bb8b08d3511d7f4ad35c5513abc6
N3v1 <n3v1010@gmail.com>
2024-03-30 08:49:59 +0100

    [Chore]: Add vercel/speedInsights

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
 import { useState } from 'react'
+import { SpeedInsights } from "@vercel/speed-insights/next"
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import { injectSpeedInsights } from '@vercel/speed-insights/*'
 
 function App() {
   const [count, setCount] = useState(0)
+
+  injectSpeedInsights();
 
   return (
     <>
